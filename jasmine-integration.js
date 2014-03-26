4600d43c0973c63adfdbbef21eda2a91fcead61d
Jordi Noguera and Taavo Smith <pair+jordi+tsmith@pivotallabs.com>
2014-03-26 18:56:33 -0400

    $iframe is now page.$el and ready is isReady

--- jasmine-integration.js
+++ jasmine-integration.js
@@ -1,30 +1,32 @@
 function Iframe() {
-  var $iframe = $("iframe#jasmine-iframe");
-  if($iframe.length == 0) {
-    $iframe = $("<iframe id='jasmine-iframe' style='width:0;height:0'></iframe>");
-    $("body").prepend($iframe);
+  this.$el = $("iframe#jasmine-iframe");
+  if(this.$el.length == 0) {
+    this.$el = $("<iframe id='jasmine-iframe' style='width:0;height:0'></iframe>");
+    $("body").prepend(this.$el);
   }
 
-  var ready;
-  $iframe[0].onload = function() { console.log("page ready"); ready = true; }
+  var isReady;
+  var oldBody;
+
+  this.$el[0].onload = function() { isReady = true; }
 
   this.setSrc = function (src) {
-    ready = false;
-    $iframe.attr("src", src);
+    isReady = false;
+    this.$el.attr("src", src);
   };
 
   this.click = function(selector) {
-    ready = false;
+    isReady = false;
     this.find(selector)[0].dispatchEvent(new Event("click"));
   };
 
   this.find = function(selector) {
-    return $iframe.contents().find(selector);
+    return this.$el.contents().find(selector);
   };
 
   this.ready = function(fun) {
     var that = this;
-    if(!ready) {
+    if(!isReady) {
       setTimeout(function() { that.ready(fun) }, 200);
     } else {
       fun();
@@ -52,13 +54,13 @@ function Iframe() {
   };
 
   this.show = function() {
-    $iframe.css("width", $(document).width());
-    $iframe.css("height", $(document).height());
+    this.$el.css("width", $(document).width());
+    this.$el.css("height", $(document).height());
   };
 
   this.hide = function() {
-    $iframe.css("width", 0);
-    $iframe.css("height", 0);
+    this.$el.css("width", 0);
+    this.$el.css("height", 0);
   };
 
   this.body = function() {
@@ -68,7 +70,7 @@ function Iframe() {
 
 function visit(path) {
   var iframe = new Iframe();
-  iframe.setSrc("http://localhost:8888" + path)
+  iframe.setSrc("http://localhost:8888" + path);
 
   return iframe;
 }
