30a8863df2b7ded8d3f18b1dc581ce1685d7f8c0
Abhinav Shrestha and Jordi Noguera <pair+ashrestha+jordi@pivotallabs.com>
2014-04-02 12:21:10 -0400

    optionally run application server before starting jasmine server

--- tasks/jasmine-integrations.js
+++ tasks/jasmine-integrations.js
@@ -1,8 +1,20 @@
 module.exports = function(grunt) {
-  var sh = require('execSync');
+  var sh = require('execSync'),
+      exec = require('child_process').exec,
+      yaml = require('js-yaml'),
+      fs = require("fs"),
+      jasmine_yaml = yaml.safeLoad(fs.readFileSync('config/jasmine.yml', 'utf8'));
+
 
   grunt.registerTask("jasmine:server", "Run jasmine server", function() {
     var command = "node node_modules/jasmine-integration/server.js";
+    if(jasmine_yaml.server_command) {
+      exec(jasmine_yaml.server_command, function(err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+      });
+    }
+
     sh.run(command);
   })
 }
