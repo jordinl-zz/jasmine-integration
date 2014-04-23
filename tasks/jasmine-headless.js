69cf85e0b6f39e779c2e2b51177c21abddc908dd
Jordi Noguera <jordinoguera83@gmail.com>
2014-04-23 20:02:42 +0200

    Forgot to run app server in headless mode

--- tasks/jasmine-headless.js
+++ tasks/jasmine-headless.js
@@ -1,8 +1,18 @@
 module.exports = function(grunt) {
   var sh = require('execSync'),
-      exec = require('child_process').exec;
+      exec = require('child_process').exec,
+      yaml = require('js-yaml'),
+      fs = require("fs"),
+      jasmine_yaml = yaml.safeLoad(fs.readFileSync('config/jasmine.yml', 'utf8'));
 
   grunt.registerTask("jasmine:server:ci", "Run jasmine server headless mode", function() {
+    if(jasmine_yaml.server_command) {
+      exec(jasmine_yaml.server_command, function(err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+      });
+    }
+
     exec("node node_modules/jasmine-integration/src/server.js", function(err, stdout, stderr) {
       console.log(stdout);
       console.log(stderr);
