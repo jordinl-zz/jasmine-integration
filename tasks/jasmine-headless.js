e70963f6ee2282187c3e0c0bbdec516085e8590a
Jordi Noguera <jordinoguera83@gmail.com>
2014-05-01 21:20:30 -0400

    kill processes after headless tests have run

--- tasks/jasmine-headless.js
+++ tasks/jasmine-headless.js
@@ -5,14 +5,16 @@ module.exports = function(grunt) {
       options = "";
 
   grunt.registerTask("jasmine:server:ci", "Run jasmine server headless mode", function() {
+    var serverProcess, jasmineProcess;
+
     if(configuration.server_command) {
-      exec(configuration.server_command, function(err, stdout, stderr) {
+      serverProcess = exec(configuration.server_command, function(err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
       });
     }
 
-    exec("node node_modules/jasmine-integration/src/server.js", function(err, stdout, stderr) {
+    jasmineProcess = exec("node node_modules/jasmine-integration/src/server.js", function(err, stdout, stderr) {
       console.log(stdout);
       console.log(stderr);
     });
@@ -22,6 +24,12 @@ module.exports = function(grunt) {
     }
 
     var code = sh.run("phantomjs node_modules/jasmine-integration/src/phantom-jasmine.js" + options);
+
+    if(serverProcess) {
+      serverProcess.kill();
+    }
+    jasmineProcess.kill();
+    
     if(code !== 0) {
       grunt.fail.fatal("Tests failed!", code);
     }
